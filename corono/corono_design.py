6018a5354b7fcca3042ba567c90959fc50c5be6c
astromam <mamadou.ndiaye@oca.eu>
2018-07-04 11:45:14 +0200

    code factorization for APLC2d
    factorization of the corono field computation function

--- corono/corono_design.py
+++ corono/corono_design.py
@@ -2208,14 +2208,13 @@ class APLC2d(Coronagraph):
             
         field_Dtmp = np.zeros((self.nlam,self.nImg2d,self.nImg2d), 
                                   dtype='complex128')
-            
-        if self.Pupil2dSym == False: 
-            for i in range(self.nlam):
-                if self.OPDmap2d is None:
-                    field = field_A
-                else:
-                    field = field_A[i]
-                    
+
+        for i in range(self.nlam):
+            if self.OPDmap2d is None:
+                field = field_A
+            else:
+                field = field_A[i]
+            if self.Pupil2dSym == False:
                 field_B       = self.mask2d*sft(field, self.nFPM, self.mB_t[i], 
                                                 CtrBtwnPix=self.CtrBtwnPix)
                 field_C       = field - isft(field_B, self.nPup, self.mB_t[i], 
@@ -2223,21 +2222,15 @@ class APLC2d(Coronagraph):
                 field_L       = field_C*self.LyotStop2d
                 field_Dtmp[i] = sft(field_L, self.nImg2d, self.mD_t[i], 
                           CtrBtwnPix=self.CtrBtwnPix2)
-        else:
-            for i in range(self.nlam):
-                if self.OPDmap2d is None:
-                    field = field_A
-                else:
-                    field = field_A[i]
-                
+            else:
                 field_B       = self.mask2d*sft_even(field, self.nFPM, self.mB_t[i], 
                                                 CtrBtwnPix=self.CtrBtwnPix)
                 field_C       = field - isft_even(field_B, self.nPup, self.mB_t[i], 
                                                CtrBtwnPix=self.CtrBtwnPix)
                 field_L       = field_C*self.LyotStop2d
                 field_Dtmp[i] = sft_even(field_L, self.nImg2d, self.mD_t[i], 
-                          CtrBtwnPix=self.CtrBtwnPix2)            
-            
+                          CtrBtwnPix=self.CtrBtwnPix2)
+                                   
         return field_Dtmp   
 
 
