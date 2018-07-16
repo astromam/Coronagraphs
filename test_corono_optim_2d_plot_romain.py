b9d35f4c9823fd80cf77d6077283c13ec2cf3b64
astromam <mamadou.ndiaye@oca.eu>
2018-07-16 16:33:53 +0200

    flle cleaning

--- test_corono_optim_2d_plot_romain.py
+++ test_corono_optim_2d_plot_romain.py
@@ -127,20 +127,6 @@ print('problem definition time       : {0:.2f}s'.format(t1-t0))
 """
 Read files
 """
-#fdir = Path('./results/2D/dat_pyth').resolve() / pupil_name
-#
-#if corono_name == 'SP':
-#    fname_gen  = 'SP00_IWA={rho0}_OWA={rho1}_BW={bw}_nlam={nlam:02d}_C={cDarkHole:.1f}_2D_nPup={nPup:04d}_{problem_name}_{solver}.fits'
-#else:
-#    fname_gen  = 'APLC_IWA={rho0}_OWA={rho1}_BW={bw}_nlam={nlam:02d}_C={cDarkHole:.1f}_2D_nPup={nPup:04d}_{problem_name}_{solver}.fits'
-#
-#fpath = fdir / fname_gen.format(**{key: corono0.params[key] for key in corono0.params})
-#
-#print('{0}'.format(fpath))
-#
-#Apod1_2d = fits.getdata(fpath,)
-
-
 fdir = Path('./results/2D/dat_pyth').resolve() / pupil_name
 
 fname = problem1.get_filename()
