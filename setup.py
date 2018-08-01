ffc3f56e742b407b5a087f545e3babea10266b92
astromam <mamadou.ndiaye@oca.eu>
2018-08-01 11:55:25 +0200

    update of the name in setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='Coronagraphs',  # Required
+    name='lyot-style-corono',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
