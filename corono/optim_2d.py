f3600d9eeb6a2977d937ce6219274850225e8312
astromam <mamadou.ndiaye@oca.eu>
2018-08-28 17:18:28 +0200

    add sparse option for scipy.optimize.linprog in optim1d and optim2d

--- corono/optim_2d.py
+++ corono/optim_2d.py
@@ -331,7 +331,7 @@ class ProblemMatrix(object):
             bds[:,1] = 1.
             sol=scipy.optimize.linprog(self.c,self.A.T,self.b,
                                        method='interior-point',
-                                       bounds=bds)
+                                       bounds=bds, options={'sparse':False})
             self.Apod[self.idx_pup]=sol.x
 
         t1 = time.time()
--- scripts/2D/test_corono_optim_2d_solve-save.py
+++ scripts/2D/test_corono_optim_2d_solve-save.py
@@ -25,7 +25,7 @@ Parameters
 # Telescope name
 pupil_name   = 'sbr' # 'vlt' or 'sbr' or 'lvr'
 problem_name = 'MaxContrastL1' # 'MaxTau' # ,'MaxContrastLinf' # 'MaxContrastL1' #
-solver       = 'stdgrb' #,'stdgrb' #  'gurobipy', 'scipy.linprog'
+solver       = 'xxx' #,'stdgrb' #  'gurobipy', 'scipy.linprog'
 slvLogToConsole = 0
 slvCrossover    = 0
 slvMethod       = 2
