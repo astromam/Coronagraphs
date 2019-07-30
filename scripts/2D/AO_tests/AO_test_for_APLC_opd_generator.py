8322935d940021fe4f70c7d833588580c2f95d09
astromam <mamadou.ndiaye@oca.eu>
2019-07-30 15:29:27 +0200

    Update AO_test_for_APLC_opd_generator.py

--- scripts/2D/AO_tests/AO_test_for_APLC_opd_generator.py
+++ scripts/2D/AO_tests/AO_test_for_APLC_opd_generator.py
@@ -17,42 +17,63 @@ from pathlib import Path
 from astropy.io import fits
 import vigan.ao as ao
 #from pyzelda.utils import aperture
+import time
 
 #%%
 """
 ### Parameters
 """
 ### Dimensions for csv data file
-nVal = 120
 nParams = 5
+nPSD    = 120
+kPSD    = 1
+iPSD    = 0
 
 ### Pupil dimension
 nPup = 384
 
-#%%
-"""
-### Paths
-"""
-fdir = Path('/Users/mndiaye/Dropbox/python/Coronagraphs/data/2D/turbulence/').resolve()
-fname = 'Obs-Sequence-Parameters.csv'
-fpath = fdir / fname
+### OPD number
+nmap = 1000
+
 
 #%%
 """
-### Read csv files from Alexis 
+### File reading paths
 """
-DATA = np.zeros((nVal,nParams))
+fdir_psd_csv  = Path('/Users/mndiaye/Dropbox/python/Coronagraphs/data/2D/turbulence/').resolve()
+fname_psd_csv = 'Obs-Sequence-Parameters.csv'
+fpath_psd_csv = fdir_psd_csv / fname_psd_csv
+
+#%%
+"""
+### File saving paths from the generated data
+"""
+### Save PSD temporal evolution
+fdir_opd = Path('../../../data/2D/AO_tests/').resolve()
+
+fname_psd = 'residual_turbulence_psd_arr_nPup={0}.fits'.format(nPup)
+fpath_psd = fdir_opd / fname_psd 
+
+fname_opd = 'AOres_opd_nPup={0}_iPSD={1:04d}_nmap={2:04d}.fits'.format(nPup,iPSD,nmap)
+fpath_opd = fdir_opd / fname_opd 
+
+
+#%%
+"""
+### Read csv files from Alexis (adpated from Elodie Choquet's file)
+"""
+DATA = np.zeros((nPSD,nParams))
 # DATA[:,0] = zenith distance [deg]
 # DATA[:,1] = seeing ['']
 # DATA[:,2] = R0 [m]
 # DATA[:,3] = wind speed [m/s]
 # DATA[:,4] = wind direction [deg]
 cc = 0
-with open(fpath, 'r') as csvFile:
+with open(fpath_psd_csv, 'r') as csvFile:
    reader = csv.reader(csvFile)
    for row in reader:
        if cc>0:
-           print(row[0],row[1],row[2],row[3],row[4])
+#           print(row[0],row[1],row[2],row[3],row[4])
            DATA[cc-1]=row
        cc = cc+1
 csvFile.close()
@@ -61,21 +82,21 @@ csvFile.close()
 """
 ### Array of residual turbulence PSD
 """
-residual_turbulence_psd_arr = np.zeros((nVal, 2*nPup, 2*nPup))
+residual_turbulence_psd_arr = np.zeros((kPSD, 2*nPup, 2*nPup))
 
-for iVal in range(nVal):    
+for iPSD in range(kPSD):    
 
     """
     ### Turbulence parameters
     """
-    seeing   = DATA[iVal,1]
+    seeing   = DATA[iPSD,1]
     L0       = 25
     z        = [2.5, 10]#[  0,   4, 16]
     Cn2      = [80, 20]#[ 55,  35, 10]
-    v        = [DATA[iVal,3], 21.2]#[  8,  10, 15]
-    arg_v    = [DATA[iVal,4], -23]#[  0, -45,  0]
+    v        = [DATA[iPSD,3], 21.2]#[  8,  10, 15]
+    arg_v    = [DATA[iPSD,4], -23]#[  0, -45,  0]
     mag      = 3.29
-    zenith   = DATA[iVal,0]
+    zenith   = DATA[iPSD,0]
     azimuth  = 71
     spaf     = 0.5
     img_wave = 1.593e-6
@@ -85,41 +106,40 @@ for iVal in range(nVal):
     """
     ### Residual turbulence PSD computation
     """
-    residual_turbulence_psd_arr[iVal] = ao.residual_screen_sphere(seeing, L0, z, Cn2, v, arg_v, mag, zenith, azimuth, 
+    t0 = time.time()
+    residual_turbulence_psd_arr[iPSD] = ao.residual_screen_sphere(seeing, L0, z, Cn2, v, arg_v, mag, zenith, azimuth, 
                                                             spat_filter=spaf, img_wave=img_wave, dim_pup=nPup,
-                                                            n_screen=100, fit=True, servo=True, alias=True, noise=True,
+                                                            n_screen=nmap, fit=True, servo=True, alias=True, noise=True,
                                                             diff_refr=True, psd_only=True, seed=seed)
+    t1 = time.time()
+    print('\nPSD - exec time: {0}s\n'.format(t1-t0))
 
-    if iVal == 0:
-        residual_turbulence_fourier = ao.residual_screen_sphere(seeing, L0, z, Cn2, v, arg_v, mag, zenith, azimuth, 
+    if iPSD == 0:
+        t0 = time.time()
+        residual_turbulence_opd_arr = ao.residual_screen_sphere(seeing, L0, z, Cn2, v, arg_v, mag, zenith, azimuth, 
                                                         spat_filter=spaf, img_wave=img_wave, dim_pup=nPup,
-                                                        n_screen=100, fit=True, servo=True, alias=True, noise=True,
+                                                        n_screen=nmap, fit=True, servo=True, alias=True, noise=True,
                                                         diff_refr=True, psd_only=False, seed=seed)
-        residual_turbulence_fourier  = residual_turbulence_fourier[..., nPup:2*nPup, nPup:2*nPup]
-#        residual_turbulence_fourier *= pupil_saxo  
+        residual_turbulence_opd_arr  = residual_turbulence_opd_arr[..., nPup:2*nPup, nPup:2*nPup]
+#        residual_turbulence_opd_arr *= pupil_saxo
+        t1 = time.time()
+        print('\nphase screen - exec time: {0}s\n'.format(t1-t0))
     
 #%%
 """
-### Save 
+### Save PSD
 """
-### Save PSD temporal evolution
-fdir = Path('/Users/mndiaye/Desktop/').resolve()
-fname = 'residual_turbulence_psd_arr_nPup={0}.fits'.format(nPup)
-fpath = fdir / fname 
-#
-fits.writeto(fpath, residual_turbulence_psd_arr, overwrite=True)
+#fits.writeto(fpath_psd, residual_turbulence_psd_arr, overwrite=True)
 
-fdir = Path('/Users/mndiaye/Desktop/').resolve()
-fname = 'residual_turbulence_phasescreen_arr_nPup={0}_iPSD={1}.fits'.format(nPup,0)
-fpath = fdir / fname 
-#
-fits.writeto(fpath, residual_turbulence_fourier, overwrite=True)
-
-
+#%%
+"""
+### Save OPD maps from PSDs
+"""
+#fits.writeto(fpath_opd, residual_turbulence_opd_arr, overwrite=True)
     
 #%%
 """
-### Display PSD for iVal=0
+### Display PSD for iPSD=0
 """
 pl.figure(2)
 pl.imshow(np.log10(residual_turbulence_psd_arr[0]), cmap = 'inferno')
