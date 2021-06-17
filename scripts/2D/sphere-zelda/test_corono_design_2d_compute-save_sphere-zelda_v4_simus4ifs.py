c0320b28697da717d2fdbcd54221fafa6f258a38
Mamadou <mamadou.ndiaye@oca.eu>
2021-06-18 00:00:52 +0200

    finalize adding star and planet spectra

--- scripts/2D/sphere-zelda/test_corono_design_2d_compute-save_sphere-zelda_v4_simus4ifs.py
+++ scripts/2D/sphere-zelda/test_corono_design_2d_compute-save_sphere-zelda_v4_simus4ifs.py
@@ -20,8 +20,19 @@ from pyzelda.utils import aperture, imutils, zernike
 from pathlib import Path
 from astropy.io import fits
 import corono as coro
+import logging
+
+import scipy.interpolate as interpolate
+import astropy.units as u
+
+from vigan.astro import skycalc
 
 import matplotlib.pyplot as plt
+
+# logging
+logging.basicConfig(format='[%(asctime)s - %(process)6s - %(levelname)-8s] %(message)s', level='INFO')
+_log = logging.getLogger(__name__)
+
 
 #%% APLC2d tests
 """
@@ -36,6 +47,10 @@ Pupil2dSym  = False
 # Telescope characteristics
 dAper     = 8
 Fratio    = 40
+
+# telescope parameters (HSIM values)
+COtel = 0.14      # central obscuration
+area  = np.pi*(dAper/2)**2*(1 - COtel**2)    # [m^2]
 
 # Focal plane mask 
 mas2rad   = np.pi/(180.*3600*1000) # Conversion factor from mas to rads
@@ -81,7 +96,18 @@ kwd_pla = True
 sep_mas_p   = 12.25*16  #5*pscale      # planet separation in mas
 theta_deg_p = 0  # planet position angle in degrees
 
+# observation parameters
+exposure  = 0      # exposure number in the sequence
+airmass   = 1.2    # airmass for exposure
+DIT       = 1      # sec
 
+# telescope and instrument transmission]
+tel_transmission = 1
+inst_transmission = 1
+
+# Noise
+kwd_noi = False
+std_ron = 10 # photo-electrons
     
 #%%
 """
@@ -175,6 +201,7 @@ fdir_pupils  = fdir / 'data' / '2D' / 'pupils' / 'SPHERE'
 fdir_zelda   = fdir / 'data' / '2D' / 'ZELDA' / str_date / str_obs  
 fdir_saxo    = fdir / 'data' / '2D' / 'ZELDA' / '2018-04-03'
 fdir_spectra = fdir / 'data' / '2D' / 'package_simu_spectra'
+fdir_sky     = fdir / 'data' / '2D' / 'skytable'
 
 if kw_aberr:
     fdir_results = fdir / 'results' / '2D' / 'data' / 'SPHERE' / str_aberr / str_date / str_obs / str_saxo / str_corr  
@@ -237,7 +264,7 @@ star_dist   = 50
 star_band   = 'H'
 star_wv_res = 1000
 
-plnt_mass   = 5
+plnt_mass   = 1
 plnt_age    = 20
 plnt_dist   = 50
 plnt_wv_res = 1000
@@ -259,6 +286,7 @@ else:
 fname_spectra_star = 'sphplus_medres_{}_{:.1f}MSun_{}Myr_{}pc_{:.1f}_{:.1f}_mic_R{}.fits'.format(star_SpT, star_mass, int(round(star_age)), int(round(star_dist)), star_wv_min, star_wv_max, int(round(star_wv_res)))
 fname_spectra_plnt = 'sphplus_medres_{}MJup_{}Myr_{}pc_{:.1f}_{:.1f}_mic_R{}.fits'.format(int(round(plnt_mass)), int(round(plnt_age)), int(round(plnt_dist)), plnt_wv_min, plnt_wv_max,int(round(plnt_wv_res)))
 
+fname_spectra_tell = 'sphplus_medres_teltransm_{:.1f}_{:.1f}_mic_R{}.fits'.format(star_wv_min, star_wv_max, int(round(star_wv_res)))
 
 #%%
 """
@@ -266,6 +294,71 @@ fname_spectra_plnt = 'sphplus_medres_{}MJup_{}Myr_{}pc_{:.1f}_{:.1f}_mic_R{}.fit
 """
 fpath_spectra_star = fdir_spectra / fname_spectra_star
 fpath_spectra_plnt = fdir_spectra / fname_spectra_plnt
+fpath_spectra_tell = fdir_spectra / fname_spectra_tell
+
+#%%
+"""
+### Spectral binning function
+"""
+def spectral_binning(wave, dwave, obj_wave, obj_phot):
+    '''
+    Spectral binning of a spectrum on an irregular wavelength grid
+
+    The function expects arrays with units but ultimately works with
+    unitless arrays to avoid some conversion issues and to speed 
+    things up
+
+    Parameters
+    ----------
+    wave : array
+        Wavelength array, in micron
+
+    dwave : array
+        Wavelength bins array, in micron
+
+    obj_wave : array
+        Wavelength array that needs binning, in micron
+
+    obj_phot : array
+        Photon array that needs binning, in phot/s/m^2/micron
+
+    Returns
+    -------
+    obj_phot_bin : array
+        Binned photon array, in phot/s/m^2
+    '''
+
+    # work with unitless variables
+    wave_ul     = wave.to('micron').value
+    dwave_ul    = dwave.to('micron').value
+    obj_wave_ul = obj_wave.to('micron').value
+    obj_phot_ul = obj_phot.to('ph s**-1 m**-2 micron**-1').value
+
+    # spectral bins bounds
+    wave_min_ul = wave_ul
+    wave_max_ul = np.append(wave_ul[1:], wave_ul[-1]+dwave_ul[-1])
+
+    # combined wavelength vector
+    obj_wave_new_ul  = np.unique(np.append(obj_wave_ul, [wave_min_ul, wave_max_ul]))
+    obj_dwave_new_ul = np.diff(obj_wave_new_ul)
+    obj_wave_new_ul  = obj_wave_new_ul[:-1]
+
+    # interpolate Flambda values on new combined wavelength vector
+    interp_phot  = interpolate.interp1d(obj_wave_ul, obj_phot_ul)
+    obj_phot_new_ul = interp_phot(obj_wave_new_ul)        # ph/s/m2/micron
+
+    # integrated in individual bins
+    obj_phot_int_ul = obj_phot_new_ul * obj_dwave_new_ul  # ph/s/m2
+
+    # sum in final bins
+    obj_phot_bin_ul = np.zeros(len(wave))
+    for idx, (wmin, wmax) in enumerate(zip(wave_min_ul, wave_max_ul)):
+        ii = np.where((wmin <= obj_wave_new_ul) & (obj_wave_new_ul < wmax))
+        obj_phot_bin_ul[idx] = obj_phot_int_ul[ii].sum()
+
+    # reapply units
+    return obj_phot_bin_ul * u.ph * u.s**-1 * u.m**-2
+
   
 #%% 
 """
@@ -362,6 +455,7 @@ corono0  = coro.design.APLC2d(**params)
 """          
 str_common = '_mono_nmap{:05d}_nlam{:04d}_saxomap_i{:05d}_f{:05d}_band{}'.format(nmap, nlam, saxomap_i, saxomap_f,band)
 str_offaxis = '_sep{:04d}mas'.format(int(round(sep_mas_p)))
+str_sphplus = '_{}_{}dMSun_{}MJup_{}Myr'.format(star_SpT, int(round(star_mass*10)), int(round(plnt_mass)), int(round(star_age)))
 
 # filepaths for the images
 fname_direct_mono_img_f     = 'direct' + str_common + '_img_f.fits'
@@ -384,6 +478,12 @@ fname_direct_mono_img_fp = 'direct' + str_common + '_img_f' + str_offaxis + '.fi
 fname_corono_mono_img_fp = 'corono' + str_common + '_img_f' + str_offaxis + '.fits'
 fpath_direct_mono_img_fp = fdir_results / fname_direct_mono_img_fp
 fpath_corono_mono_img_fp = fdir_results / fname_corono_mono_img_fp
+
+# filepath for the images with star and planet
+fname_direct_cube = 'direct' + str_common + '_img_f' + str_sphplus + '.fits'
+fname_corono_cube = 'corono' + str_common + '_img_f' + str_sphplus + '.fits'
+fpath_direct_cube = fdir_results / fname_direct_cube
+fpath_corono_cube = fdir_results / fname_corono_cube
 
 
 #%%
@@ -462,10 +562,18 @@ for ilam in range(nlam):
 
 #%%
 """
-### Photometry and spectra for the star 
+### Unit conversion
+"""
+area     *= u.m**2
+DIT      *= u.s
+
+#%%
+"""
+### Photometry and spectra for the star, the planet and the telluric lines 
 """
 star_spec = fits.getdata(fpath_spectra_star)
 plnt_spec = fits.getdata(fpath_spectra_plnt)
+tell_spec = fits.getdata(fpath_spectra_tell)
 
 star_wave = star_spec[0]
 star_flux = star_spec[1]
@@ -473,7 +581,8 @@ star_flux = star_spec[1]
 plnt_wave = plnt_spec[0]
 plnt_flux = plnt_spec[1]
 
-
+tell_wave = tell_spec[0]
+tell_flux = tell_spec[1]
 
 #%%
 """
@@ -487,6 +596,131 @@ plt.plot(plnt_wave, np.log10(plnt_flux))
 plt.show()
 
 
+#%%
+
+star_wave *= u.um
+star_flux *= u.W / u.m**2 / u.um
+star_phot = star_flux.to('ph s**-1 m**-2 micron**-1', equivalencies=u.spectral_density(star_wave))
+
+#%%
+
+plnt_wave *= u.um
+plnt_flux *= u.W / u.m**2 / u.um
+plnt_phot = plnt_flux.to('ph s**-1 m**-2 micron**-1', equivalencies=u.spectral_density(plnt_wave))
+
+#%%
+tell_wave *= u.um
+#tell_flux *= u.dimensionless_unscaled
+
+
+#%%
+wv_um_t = wv_t * 1e6
+dwv_um_t = dwv_t * 1e6 
+
+#%%
+wv_um_t *= u.um
+dwv_um_t *= u.um
+
+#%%
+"""
+### bin over final wavelength grid
+"""
+
+star_phot = spectral_binning(wv_um_t, dwv_um_t, star_wave, star_phot)
+plnt_phot = spectral_binning(wv_um_t, dwv_um_t, plnt_wave, plnt_phot)
+
+#tell_phot = transmission_spectral_binning(wv_um_t, dwv_um_t, tell_wave, tell_flux)
+
+#%%
+"""
+### apply stellar photometry
+"""
+direct_mono_img_f_obs = direct_mono_img_f * u.dimensionless_unscaled 
+direct_mono_img_f_obs *= star_phot[:, None, None]
+
+corono_mono_img_f_obs = corono_mono_img_f * u.dimensionless_unscaled 
+corono_mono_img_f_obs *= star_phot[:, None, None]
+
+#%%
+"""
+### apply planetary photometry
+"""
+direct_mono_img_fp_obs = direct_mono_img_fp * u.dimensionless_unscaled 
+direct_mono_img_fp_obs *= plnt_phot[:, None, None]
+
+corono_mono_img_fp_obs = corono_mono_img_fp * u.dimensionless_unscaled 
+corono_mono_img_fp_obs *= plnt_phot[:, None, None]
+
+#%%
+"""
+### add stellar and planetary signal
+"""
+direct_cube = direct_mono_img_f_obs + direct_mono_img_fp_obs
+corono_cube = corono_mono_img_f_obs + corono_mono_img_fp_obs
+
+#%%
+"""
+### atmospheric transmission and emission
+"""
+_log.info('Add sky transmission and emission')
+fname_sky = fdir_sky / f'skytable_airmass={airmass:.2f}.fits'
+if fname_sky.exists():
+     sky = fits.getdata(fname_sky)
+else:
+     sky = skycalc.sky_model(observatory='3060', airmass=airmass,
+                             pwv_mode='pwv', season=0, time=0, pwv=2.5, msolflux=130.0,
+                             incl_moon='N', incl_starlight='Y', incl_zodiacal='N',
+                             incl_loweratm='Y', incl_upperatm='Y', incl_airglow='Y',
+                             vacair='vac', wmin=950, wmax=1801,
+                             wgrid_mode='fixed_spectral_resolution', wres=star_wv_res)
+     fits.writeto(fname_sky, sky)
+
+# transmission
+sky_wave = sky['lam'] * u.nm
+sky_trsm = sky['trans']
+
+wave_min = wv_t[0] *u.m
+wave_max = wv_t[-1] *u.m
+
+ii = (wave_min <= sky_wave) & (sky_wave <= wave_max)
+sky_wave = sky_wave[:nlam]
+sky_trsm = sky_trsm[:nlam]
+
+direct_cube *= sky_trsm[:, None, None]
+corono_cube *= sky_trsm[:, None, None]
+
+#%%
+"""
+### apply telescope and instrumental noises
+"""
+_log.info('Add instrumental noises')
+
+direct_cube = direct_cube * tel_transmission * inst_transmission
+corono_cube = corono_cube * tel_transmission * inst_transmission
+
+direct_cube = direct_cube * DIT * area   # phot/s/m2 ==> phot
+corono_cube = corono_cube * DIT * area   # phot/s/m2 ==> phot
+
+if kwd_noi:
+    _log.info('Add photon and readout noise')
+    # photon noise
+    direct_Int_phn = np.random.poisson(lam =direct_cube.value)
+    corono_Int_phn = np.random.poisson(lam =corono_cube.value)
+    
+    # readout noise
+    direct_Int_ron = np.random.normal(0.0, std_ron, size=(nlam,nImg2d,nImg2d))
+    corono_Int_ron = np.random.normal(0.0, std_ron, size=(nlam,nImg2d,nImg2d))
+
+    # total noise
+    direct_cube = direct_Int_phn + direct_Int_ron
+    corono_cube = corono_Int_phn + corono_Int_ron
+    
+    # apply stellar photometry
+    direct_cube *= u.dimensionless_unscaled
+    corono_cube *= u.dimensionless_unscaled
+else:
+    _log.warning(' ==> no noise added!')
+ 
 
 #%% saving of the images
 """
@@ -495,10 +729,12 @@ plt.show()
 if do_sav:
     data_list = [direct_mono_img_f,corono_mono_img_f,direct_mono_prf_avg_f,
                  corono_mono_prf_avg_f,direct_mono_prf_std_f,corono_mono_prf_std_f,
-                 direct_mono_img_fp, corono_mono_img_fp]
+                 direct_mono_img_fp, corono_mono_img_fp,
+                 direct_cube.value, corono_cube.value]
     fpath_list = [fpath_direct_mono_img_f,fpath_corono_mono_img_f,fpath_direct_mono_prf_avg_f,
                   fpath_corono_mono_prf_avg_f,fpath_direct_mono_prf_std_f,fpath_corono_mono_prf_std_f,
-                  fpath_direct_mono_img_fp, fpath_corono_mono_img_fp]
+                  fpath_direct_mono_img_fp, fpath_corono_mono_img_fp,
+                  fpath_direct_cube, fpath_corono_cube]
     nlist = len(data_list)
     
     for ilist in range(nlist):
@@ -520,5 +756,5 @@ if do_sav:
     
         hdu = fits.HDUList([hdu_prim, hdu_img, hdu_wave])
     
-        hdu.writeto(fpath_list[ilist], overwrite=True)    
-    
+        hdu.writeto(fpath_list[ilist], overwrite=True)   
+            
