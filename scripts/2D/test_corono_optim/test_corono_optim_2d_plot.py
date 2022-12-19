1e104c33b2360b80ea38a94bfbd9d81b70ececd5
Mamadou <mamadou.ndiaye@oca.eu>
2022-12-19 14:54:10 +0100

    change solver

--- scripts/2D/test_corono_optim/test_corono_optim_2d_plot.py
+++ scripts/2D/test_corono_optim/test_corono_optim_2d_plot.py
@@ -35,7 +35,7 @@ if True:
     corono_name  = 'APLC' # 'SP' or 'APLC'
     pupil_name   = 'sbr' # 'vlt' or 'sbr' or 'lvr'
     problem_name = 'MaxContrastLinf' # 'MaxContrastL1' #'MaxTau' # , 'MaxContrastLinf' # #  
-    solver       = 'stdgrb' # 'stdgrb' #  'gurobipy', 'scipy.linprog'
+    solver       = 'gurobipy' # 'stdgrb' #  'gurobipy', 'scipy.linprog'
     
     MinIsland   = False
     FirstDerGlobalLim = 1.
