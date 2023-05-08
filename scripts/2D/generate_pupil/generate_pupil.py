e7b7f4d85f6183e29454c86a8250bedede728d32
Mamadou <mamadou.ndiaye@oca.eu>
2023-05-09 00:04:41 +0200

    modify code to account for the differences in aperture
    beware of the differences between pyzelda.utils.aperture and vigan.optics.aperture

--- scripts/2D/generate_pupil/generate_pupil.py
+++ scripts/2D/generate_pupil/generate_pupil.py
@@ -11,7 +11,8 @@ License: MIT license
 
 # Initialization
 import numpy as np
-import pyzelda.utils.aperture as aperture
+#import pyzelda.utils.aperture as aperture
+from vigan.optics import aperture
 
 import pylab as pl
 from pathlib import Path
@@ -25,15 +26,24 @@ import xaosim
 ### Parameters
 """
 pupil_name = 'vlt_btw'
-nPup= 100#384
-do_fits = True
+do_dead_act = False
+nPup= 400#384
+do_fits = False
+
 
 #%%
 """
 ### generation of a VLT like pupil
 """
+x_deadact = [0.1534, -0.0984, -0.1963, 0.2766, 0.3297]
+y_deadact = [-0.0768, -0.1240, -0.3542, -0.2799, -0.2799] 
+
 if pupil_name == 'vlt':
-    pupil = aperture.vlt_pupil(nPup, nPup, dead_actuator_diameter=0.)
+    if do_dead_act:
+        pupil = aperture.vlt_pupil(nPup, nPup, dead_actuators=[x_deadact,y_deadact],
+        dead_actuator_diameter=0.025, cpix=False)
+    else:
+        pupil = aperture.vlt_pupil(nPup, nPup, dead_actuator_diameter=0.)
 elif pupil_name == 'vlt_btw':
     pdiam, odiam = 8.00, 1.12
     thick = 0.04              # adopted spider thickness (meters)
@@ -71,8 +81,13 @@ pl.show()
 save pupil
 """
 fdir = Path('/Users/mndiaye/OneDrive - Université Nice Sophia Antipolis/data/Coronagraphs/data/2D/pupils').resolve()
-fname = f'pupil={pupil_name}_nPup={nPup}.fits'
+str_dead_act=''
+if do_dead_act and pupil_name == 'vlt':
+    str_dead_act = '_dead_act'
+
+fname = f'pupil={pupil_name}_nPup={nPup}{str_dead_act}.fits'
 fpath = fdir / fname
+print(fname)
 
 if do_fits:
     fits.writeto(fpath, pupil*1., overwrite=True)
