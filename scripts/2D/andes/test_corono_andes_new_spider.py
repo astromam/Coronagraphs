3ddce8574676641b42bfd9ec03d24ea83778b208
spang06 <spang.alain@gmail.com>
2024-09-16 16:53:28 +0200

    add comments

--- scripts/2D/andes/test_corono_andes_new_spider.py
+++ scripts/2D/andes/test_corono_andes_new_spider.py
@@ -5,6 +5,9 @@ Created on Fri Mar 31 13:26:29 2023
 
 @authors: mndiaye, asimonnin, asp
 """
+# compute normalized psf profiles intensity for coro and no coro, 
+# with or without residual opds
+# normalized to the peak intensity of the no coro, no opds pupil with lyot stop
 
 #%%
 """
