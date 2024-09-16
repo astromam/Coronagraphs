16cae53550dd8abd5e23f01859499ad03324d811
spang06 <spang.alain@gmail.com>
2024-09-16 16:55:17 +0200

    create fits cube file with ncpa in given pupil

--- scripts/2D/andes/make_phase_screens.py
+++ scripts/2D/andes/make_phase_screens.py
@@ -4,36 +4,100 @@ Created on Mon Jul 29 17:07:41 2024
 
 @author: asimmonnin, mndiaye, asp
 """
-
-
+# create sets of phase screens files with given ncpa value
 
 # %%
 
 import numpy as np
 import slow_fourier_transform as sft
+from pathlib import Path
+from astropy.io import fits
+
 
 #%%
-# Dimensions de l'image
-N = 800
-# Créer un espace de fréquence en 2D
-kx = (np.arange(N)-N//2)/(N/2)  # Fréquences spatiales pour les colonnes
+"""
+### Working directories
+"""
+user = 'Alain'
+if user == 'Alain':
+    fdir_dat   = Path('D:/Andes/Data_corono/data/').resolve()  #  fits data
+
+elif user == 'Adrien':
+    # Directory for the OPD with the corresponding seed value
+    fdir_dat   = Path(
+        '/Users/asimonnin/Desktop/PhD/Andes/Data_corono/data/').resolve()
+
+elif user == 'Mamadou':
+    fdir_base = ("/Users/mndiaye/Library/CloudStorage/"\
+                 "OneDrive-UniversitéNiceSophiaAntipolis/data/andes")
+    # Directory for the OPD with the corresponding seed value
+    fdir_dat   = Path( fdir_base / 'data' ).resolve()
+
+
+#%%
+
+std_tgt = 5e-8 #☺ target std in meters
+
+fdir_pupil = fdir_dat / 'Pupil'
+fname_elt = 'ELT_pupil_400.fits' # New pupil with new spider
+fpath_elt = fdir_pupil / fname_elt
+
+"""
+### Read pupil file
+"""
+# Read ELT pupil 
+Pupil = fits.getdata(fpath_elt,)
+
+# image dimensions, even, twice the pupil size
+N = Pupil.shape[0]*2
+
+# 2D frequency space
+kx = (np.arange(N)-N//2)/(N/2)  # spatial frequencies
 ky = (np.arange(N)-N//2)/(N/2)
 kx2, ky2 = np.meshgrid(kx, ky)
-k = np.sqrt(kx2**2 + ky2**2)  # Norme des vecteurs de fréquences
+k = np.sqrt(kx2**2 + ky2**2)  # spatial fréquencies norm vector
 
-# Appliquer la loi f^-2 pour obtenir la DSP
-# Pour éviter la division par zéro, on ajoute un petit epsilon à k
+# DSP law in f^-pwr
+# add epsilon to avoid zero division
+pwr=-2.
 epsilon = 1e-10
-dsp = 1/(k**2 + epsilon)
+dsp = 1./(k**pwr + (k==0)*epsilon)
 
-# Génération de phases aléatoires
-# Calcul des composantes complexes du spectre
+# uniform random phase generation
+# complex valued spectra
 amplitude = np.sqrt(dsp)
-random = np.random.randn(*amplitude.shape)
-opd_ff = amplitude * random
+#amplitude.shape est un tuple et *amplitude.shape sont les éléments de ce tuple
+random = np.random.uniform(low=-0.5,high=0.5,size=amplitude.shape)
+ncpa_field = amplitude * np.exp(1j*2.*np.pi*random)
 
 # Transformée de Fourier inverse pour obtenir l'image spatiale
-random_opd_f2 = np.real(sft.isft(amplitude*random,800,400))
+rndOpd_powLaw = np.real(sft.isft(ncpa_field,N,N//2))
 
-# Normalisation de l'image pour l'affichage
-opd_f2 = (random_opd_f2  - np.min(random_opd_f2 )) / (np.max(random_opd_f2 ) - np.min(random_opd_f2 ))
+# boucle extraction aleatoire d'un masque random de dimension identique
+# à la pupille
+
+# N = pupil size now
+N=N//2
+hlf=N//2
+nb_ncpa = 2048
+rnd=np.random.randn(nb_ncpa)
+xi=np.round(rnd*hlf/np.max([-np.min(rnd),np.max(rnd)])).astype(int)
+rnd=np.random.randn(nb_ncpa)
+yi=np.round(rnd*hlf/np.max([-np.min(rnd),np.max(rnd)])).astype(int)
+
+ncpa = np.ones((N,N,nb_ncpa))
+ncpa *= Pupil[:,:,None].copy()
+iok = np.nonzero(Pupil.copy())
+
+for n in np.arange(nb_ncpa):
+    
+    temp = (rndOpd_powLaw[hlf+xi[n]:hlf+N+xi[n],hlf+yi[n]:hlf+N+yi[n]]).copy()
+    ncpa[:,:,n] *= temp
+    ncpa[:,:,n] -= np.mean(ncpa[:,:,n][iok])
+    ncpa[:,:,n] *= std_tgt/np.std(ncpa[:,:,n][iok])
+
+fits.writeto(
+    (fdir_dat/('ncpa_pupil_new_'+str(int(np.rint(std_tgt*1e9)))+"nm.fits")),
+    np.transpose(ncpa,(2,0,1)), overwrite=True)
+
+
