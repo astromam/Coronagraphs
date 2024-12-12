390bba810b5d29a04a0ff5a3ef545b0a46dba997
Mamadou <mamadou.ndiaye@oca.eu>
2024-12-12 16:54:28 +0100

    import packages

--- corono/optim_1d.py
+++ corono/optim_1d.py
@@ -33,7 +33,7 @@ import scipy.optimize
 #from . import design, default  
 from .utils import update_params
 #from .optim_func import  line_search_armijo,line_search_ratio,fmin_cond,solve_closed_form,cost_function_snr,gradient_function_snr  
-#from . import design, default,utils       
+from . import design, default,utils       
 
 #%%
 """
