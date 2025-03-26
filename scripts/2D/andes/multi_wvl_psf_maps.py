3b9ae09306ceda6696ba65aad51ec98069d0451b
spang06 <spang.alain@gmail.com>
2025-03-26 16:53:17 +0100

    remove function recursive_search + new FOV definition

--- scripts/2D/andes/multi_wvl_psf_maps.py
+++ scripts/2D/andes/multi_wvl_psf_maps.py
@@ -28,31 +28,6 @@ plt.rcParams.update({'font.size': 14})  #♦  mdiaye 15!
 
 #%%
 """
-stackoverflow...
-"""
-
-def recursive_search(path: str) -> "list[str]":
-    """get all files from an absolute path
-
-    :param path: absolute path of the directory to search
-    :type path: str
-    :return: a list of all files
-    :rtype: list[str]
-    """
-    found_files = []
-    if not os.path.isdir(path):
-        raise RuntimeError(f"'{path}' is not a directory")
-    for item in os.listdir(path):
-        full_path = os.path.join(path, item)
-        if os.path.isfile(full_path):
-            found_files.append(full_path)
-        elif os.path.isdir(full_path):
-            found_files.extend(recursive_search(full_path))
-    return found_files
-
-
-#%%
-"""
 ### scaling
 """
 
@@ -65,6 +40,7 @@ mas2rad = 1/rad2mas
 # angular separation of interest in mas
 as_oi = 25.
 
+wvl = 'YJH'
 
 #%%
 """
@@ -72,12 +48,9 @@ as_oi = 25.
 """
 user = 'Alain'
 if user == 'Alain':
+    fdir_base  = Path('D:/Andes/Data_corono/').resolve()
     fdir_res   = Path('D:/Andes/Data_corono/results/').resolve()  #  fits data
     fdir_plt   = Path('D:/Andes/Data_corono/plots/').resolve()   #  plots
-
-    was_donow = '20241121161633'
-    fdir_res = fdir_res / was_donow
-    fdir_plt = fdir_plt / was_donow
 
 elif user == 'Adrien':
     # Directory for the OPD with the corresponding seed value
@@ -93,34 +66,78 @@ elif user == 'Mamadou':
     fdir_res   = Path( fdir_base / 'results' ).resolve()
     fdir_plt   = Path( fdir_base / 'plots' ).resolve()
 
-# 20241007141137 yjh 0 disp, 0 ncpa, 0 tilt
+was_donow = '20250326150540'
+fdir_res = fdir_res / was_donow
+fdir_plt = fdir_plt / was_donow
 
-# offset in lambda central/D for psf to fpm + yjh
 
-# 20241112095915 0.05
-# 20241112100035 0.10
-# 20241112100137 0.15
-# 20241112100549 0.20
-# 20241112100648 0.25
-# 20241121163003 0.25 colineaire!
-# 20241112175847 0.50
-# 20241112175935 0.75
-# 20241112180014 1.00
+# ideal with lyotstop + petaling20250321160937 0,1,2 20250321161111 1,2,3
+# 20250321144751 perfect 2nd order with lyotstop + petaling JQM
+# 20250321093529 perfect 2nd order with lyotstop + TT + petaling 2..4 vs 5..1
+# 20250321093502 perfect 2nd order with lyotstop + TT + petaling 1..3 vs 4..0
+# 20250321085350 perfect 2nd order with lyotstop + TT + petaling 0..2 vs 3..5
 
-# disp + yjh
-# 20241106094210  5 mas/µ
-# 20241024101416 10 mas/µ
-# 20241106094322 15 mas/µ
+# 20250320164637 perfect 2nd order with lyotstop + TT + petaling 2..3 vs 4..1
+# 20250320164551 perfect 2nd order with lyotstop + TT + petal 1..2 vs 3..0
+# 20250320163707 perfect 2nd order with lyotstop + TT + petals 3..5 vs 0..2?
+# 20250317172539 perfect 2nd order with lyotstop + TT
+# 20250318154752 perfect 2nd order with lyotstop + TT, JQ1 scaled 90%
+# 20250318154814 perfect 2nd order with lyotstop + TT, JQ1 scaled 80%
+# 20250318154832 perfect 2nd order with lyotstop + TT, JQ1 scaled 70%
+# 20250319083152 perfect 2nd order with lyotstop + TT, JQ1 scaled 60%
+# 20250319083224 perfect 2nd order with lyotstop + TT, JQ1 scaled 50%
+# 20250317172808 perfect 2nd order with lyotstop + petalling corr + TT
+
+# 20250313135902 perfect 2nd order coro + petalling corr
+# 20250312094239 psf no TT corr, coro TT corr
+# 20250313130757 20250313130437 corr. petal. after TT / first then TT
+# 20250312160834 perfect coro no petalling OPDs_PASSATA/OPD/WS/ASI
+# 20250310155522 20250312085754 perfect coro 2nd order full elt pupil
+
+# 20250121142815 yjh 0.9/0.37/4.0  0 disp,0 ncpa,0,tilt perfect: 20250122140220
+
+# lyot stop horizontal offset in pixels
+# 20250214100537 1
+# 20250214100614 2
+# 20250214100649 4
+# 20250214100716 8
+
+# lyot stop angular position error
+# 20250214100748 0.5
+# 20250214100828 1.0
+# 20250214100857 2.0
+
+# lyot stop vertical offset in pixels
+# 20250130104616 1
+# 20250130104747 2
+# 20250130104759 4
+# 20250130104809 8
+
+# fpm defocus in nm RMS
+# 20250210175500 10
+# 20250210175517 20
+# 20250210175531 30
+# 20250210175546 40
+# 20250210175606 50
 
 # ncpa nm rms with yjh 0mas/µ disp
-#  20241105085056 10
-#  20241105131506 20
-#  20241105155837 30
-#  20241105155850 40
-#  20241105155859 50
+# 20250122183859 10
+# 20250122184044 20
+# 20250122184053 30
+# 20250122184101 40
+# 20250122184109 50
 
-# 20241118143408 30 nm ncp, 0.25 lamC/D et 5 mas/µ bw
-# 20241121161633 30 nm ncpa, 0.25 lamC/D et 5 mas/µ bw colineaires
+# disp + yjh
+# 20250124135415 5
+# 20250124135527 10
+# 20250124135557 15
+
+# offset/tilt in mas for psf to fpm
+# 20250217154848 1
+# 20250217155004 2
+# 20250217155048 3
+# 20250217155138 4
+# 20250217155355 20
 
 
 #%%
@@ -137,22 +154,59 @@ elif user == 'Mamadou':
 #               'OPDs_PASSATA/OPD/20240313_133532-004/20240313_133532.0',
 #               'OPDs_PASSATA/OPD/20240228_112033-002/20240228_112033.0')
 # opds_dir=('OPDs_PASSATA/OPD/WS/JQ1/20240515_163822',
-#           'OPDs_PASSATA/OPD/WS/JQ1/20240517_091216',
-#           'OPDs_PASSATA/OPD/WS/JQ1/20240517_100705',
-#           'OPDs_PASSATA/OPD/WS/JQ1/20240517_103452',
-#           'OPDs_PASSATA/OPD/WS/JQ1/20240517_105822',
-#           'OPDs_PASSATA/OPD/WS/JQ1/20240517_111658',
-#           'OPDs_PASSATA/OPD/WS/JQ1/20240517_113534',
-#           'OPDs_PASSATA/OPD/WS/JQ1/20240517_121247',
-#           'OPDs_PASSATA/OPD/WS/JQ2/20240517_181033',
-#           'OPDs_PASSATA/OPD/WS/JQ2/20240517_183817',
-#           'OPDs_PASSATA/OPD/WS/JQ2/20240517_190418',
-#           'OPDs_PASSATA/OPD/WS/JQ2/20240517_192251',
-#           'OPDs_PASSATA/OPD/WS/JQ2/20240517_194126',
-#           'OPDs_PASSATA/OPD/WS/JQ2/20240517_195959',
-#           'OPDs_PASSATA/OPD/WS/JQ2/20240517_201835',
-#           'OPDs_PASSATA/OPD/WS/JQ2/20240517_203708',
-#           'OPDs_PASSATA/OPD/WS/JQM/20240509_182041/20240509_182041.0',
+          # 'OPDs_PASSATA/OPD/WS/JQ1/20240517_091216',
+          # 'OPDs_PASSATA/OPD/WS/JQ1/20240517_100705',
+          # 'OPDs_PASSATA/OPD/WS/JQ1/20240517_103452',
+          # 'OPDs_PASSATA/OPD/WS/JQ1/20240517_105822',
+          # 'OPDs_PASSATA/OPD/WS/JQ1/20240517_111658',
+          # 'OPDs_PASSATA/OPD/WS/JQ1/20240517_113534',
+          # 'OPDs_PASSATA/OPD/WS/JQ1/20240517_121247')#,
+          # 'OPDs_PASSATA/OPD/WS/JQ2/20240517_181033',
+          # 'OPDs_PASSATA/OPD/WS/JQ2/20240517_183817',
+          # 'OPDs_PASSATA/OPD/WS/JQ2/20240517_190418',
+          # 'OPDs_PASSATA/OPD/WS/JQ2/20240517_192251',
+          # 'OPDs_PASSATA/OPD/WS/JQ2/20240517_194126',
+          # 'OPDs_PASSATA/OPD/WS/JQ2/20240517_195959',
+          # 'OPDs_PASSATA/OPD/WS/JQ2/20240517_201835',
+          # 'OPDs_PASSATA/OPD/WS/JQ2/20240517_203708',
+          # 'OPDs_PASSATA/OPD/WS/JQM/20240509_182041/20240509_182041.0',
+          # 'OPDs_PASSATA/OPD/WS/JQM/20240509_183915/20240509_183915.0',
+          # 'OPDs_PASSATA/OPD/WS/JQM/20240509_191620/20240509_191620.0',
+          # 'OPDs_PASSATA/OPD/WS/JQM/20240509_193453/20240509_193453.0',
+          # 'OPDs_PASSATA/OPD/WS/JQM/20240509_195327/20240509_195327.0',
+          # 'OPDs_PASSATA/OPD/WS/JQM/20240509_201200/20240509_201200.0',
+          # 'OPDs_PASSATA/OPD/WS/JQM/20240509_203033/20240509_203033.0',
+          # 'OPDs_PASSATA/OPD/WS/JQM/20240509_204907/20240509_204907.0',
+          # 'OPDs_PASSATA/OPD/WS/JQM/20240509_210742/20240509_210742.0')#,
+          # 'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-001/JQ3/20240521_200540',
+          # 'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-004/JQ3/20240521_181105',
+          # 'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-002/JQ3/20240521_213115',
+          # 'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-005/JQ3/20240521_222747',
+          # 'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-003/JQ3/20240521_210334',
+          # 'OPDs_PASSATA/OPD/WS/JQ3/20240527_190439/JQ3/20240527_190439',
+          # 'OPDs_PASSATA/OPD/WS/JQ3/20240527_195648/JQ3/20240527_195648',
+          # 'OPDs_PASSATA/OPD/WS/JQ3/20240527_204843/JQ3/20240527_204843',
+          # 'OPDs_PASSATA/OPD/WS/JQ3/20240527_214043/JQ3/20240527_214043',
+          # 'OPDs_PASSATA/OPD/WS/JQ3/20240527_223245/JQ3/20240527_223245',
+          # 'OPDs_PASSATA/OPD/WS/JQ4/20240528_161522/JQ4/20240528_161522',
+          # 'OPDs_PASSATA/OPD/WS/JQ4/20240528_163416/JQ4/20240528_163416',
+          # 'OPDs_PASSATA/OPD/WS/JQ4/20240528_165414/JQ4/20240528_165414',
+          # 'OPDs_PASSATA/OPD/WS/JQ4/20240528_171307/JQ4/20240528_171307',
+          # 'OPDs_PASSATA/OPD/WS/JQ4/20240528_173157/JQ4/20240528_173157',
+          # 'OPDs_PASSATA/OPD/WS/JQ4/20240528_175246/JQ4/20240528_175246',
+          # 'OPDs_PASSATA/OPD/WS/JQ4/20240528_181244/JQ4/20240528_181244',
+          # 'OPDs_PASSATA/OPD/WS/JQ4/20240528_183130/JQ4/20240528_183130',
+          # 'OPDs_PASSATA/OPD/WS/JQ4/20240528_185018/JQ4/20240528_185018',
+          # 'OPDs_PASSATA/OPD/WS/JQ4/20240528_190906/JQ4/20240528_190906')
+
+# opds_dir=('OPDs_PASSATA/OPD/WS/JQM/20240509_201200/20240509_201200.0',)
+
+# opds_dir=('OPDs_PASSATA/OPD/WS/ASI',)
+
+# # ,
+# #           'perfect')
+
+# opds_dir=('OPDs_PASSATA/OPD/WS/JQM/20240509_182041/20240509_182041.0',
 #           'OPDs_PASSATA/OPD/WS/JQM/20240509_183915/20240509_183915.0',
 #           'OPDs_PASSATA/OPD/WS/JQM/20240509_191620/20240509_191620.0',
 #           'OPDs_PASSATA/OPD/WS/JQM/20240509_193453/20240509_193453.0',
@@ -160,42 +214,16 @@ elif user == 'Mamadou':
 #           'OPDs_PASSATA/OPD/WS/JQM/20240509_201200/20240509_201200.0',
 #           'OPDs_PASSATA/OPD/WS/JQM/20240509_203033/20240509_203033.0',
 #           'OPDs_PASSATA/OPD/WS/JQM/20240509_204907/20240509_204907.0',
-#           'OPDs_PASSATA/OPD/WS/JQM/20240509_210742/20240509_210742.0',
-#           'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-001/JQ3/20240521_200540',
-#           'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-004/JQ3/20240521_181105',
-#           'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-002/JQ3/20240521_213115',
-#           'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-005/JQ3/20240521_222747',
-#           'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-003/JQ3/20240521_210334',
-#           'OPDs_PASSATA/OPD/WS/JQ3/20240527_190439/JQ3/20240527_190439',
-#           'OPDs_PASSATA/OPD/WS/JQ3/20240527_195648/JQ3/20240527_195648',
-#           'OPDs_PASSATA/OPD/WS/JQ3/20240527_204843/JQ3/20240527_204843',
-#           'OPDs_PASSATA/OPD/WS/JQ3/20240527_214043/JQ3/20240527_214043',
-#           'OPDs_PASSATA/OPD/WS/JQ3/20240527_223245/JQ3/20240527_223245',
-#           'OPDs_PASSATA/OPD/WS/JQ4/20240528_161522/JQ4/20240528_161522',
-#           'OPDs_PASSATA/OPD/WS/JQ4/20240528_163416/JQ4/20240528_163416',
-#           'OPDs_PASSATA/OPD/WS/JQ4/20240528_165414/JQ4/20240528_165414',
-#           'OPDs_PASSATA/OPD/WS/JQ4/20240528_171307/JQ4/20240528_171307',
-#           'OPDs_PASSATA/OPD/WS/JQ4/20240528_173157/JQ4/20240528_173157',
-#           'OPDs_PASSATA/OPD/WS/JQ4/20240528_175246/JQ4/20240528_175246',
-#           'OPDs_PASSATA/OPD/WS/JQ4/20240528_181244/JQ4/20240528_181244',
-#           'OPDs_PASSATA/OPD/WS/JQ4/20240528_183130/JQ4/20240528_183130',
-#           'OPDs_PASSATA/OPD/WS/JQ4/20240528_185018/JQ4/20240528_185018',
-#           'OPDs_PASSATA/OPD/WS/JQ4/20240528_190906/JQ4/20240528_190906')
-# # ,
-# #           'perfect')
-opds_dir=('OPDs_PASSATA/OPD/WS/JQM/20240509_182041/20240509_182041.0',
-          'OPDs_PASSATA/OPD/WS/JQM/20240509_183915/20240509_183915.0',
-          'OPDs_PASSATA/OPD/WS/JQM/20240509_191620/20240509_191620.0',
-          'OPDs_PASSATA/OPD/WS/JQM/20240509_193453/20240509_193453.0',
-          'OPDs_PASSATA/OPD/WS/JQM/20240509_195327/20240509_195327.0',
-          'OPDs_PASSATA/OPD/WS/JQM/20240509_201200/20240509_201200.0',
-          'OPDs_PASSATA/OPD/WS/JQM/20240509_203033/20240509_203033.0',
-          'OPDs_PASSATA/OPD/WS/JQM/20240509_204907/20240509_204907.0',
-          'OPDs_PASSATA/OPD/WS/JQM/20240509_210742/20240509_210742.0')
+#           'OPDs_PASSATA/OPD/WS/JQM/20240509_210742/20240509_210742.0')
 # ,
 #           'perfect')
 
-opds_dir=('OPDs_PASSATA/OPD/WS/JQM/20240509_182041/20240509_123456.0',)
+# opds_dir=('OPDs_PASSATA/OPD/WS/JQM/20240509_182041/20240509_123456.0',)
+opds_dir=('OPDs_PASSATA/OPD/WS/JQ1/20240515_163822',)
+opds_dir=('OPDs_PASSATA/OPD/WS/JQ1/20240515_163822',
+          'OPDs_PASSATA/OPD/WS/JQM/20240509_201200/20240509_201200.0')
+opds_dir=('OPDs_PASSATA/OPD/WS/ASI_staticVib',)
+# opds_dir=('OPDs_PASSATA/OPD/WS/ASI_M1M4err',)
 
 
 #%%
@@ -210,11 +238,20 @@ for dir_nb in range(len(opds_dir)):
     ### Read psf files & create profiles
     """
     
-    #  TODO  trouver un moyen de securiser la selection des fichiers
-    
-    file_lst = sorted(
-        recursive_search(fdir_res / opd_set), key=os.path.getmtime)
+    # file_lst = sorted(
+    #     recursive_search(fdir_res / opd_set), key=os.path.getmtime)
     # , reverse=True
+    
+    cur_dir = Path(os.path.basename(opds_dir[dir_nb])).stem
+    res_dir = (fdir_res / cur_dir)
+    print(cur_dir, res_dir)
+    
+    if os.path.isdir(res_dir):
+        
+        os.makedirs(fdir_plt / opd_set, exist_ok=True)
+
+    
+    file_lst = os.listdir(res_dir)
 
     if opd_set != 'perfect':
         
@@ -251,27 +288,29 @@ for dir_nb in range(len(opds_dir)):
     nL = len(lam_lst)
     
     nImg = head_psf['NIMG']
+    pscale = head_psf['PSCL']
     D = head_psf['DIAM']
     mB = head_psf['SFPM']
-    lam_c = head_psf['LMBD']
-      
+    # lam_c = head_psf['LMBD']
+    hlf_fov = nImg * pscale / 2.
+
     # stackoveflow...
     a_ = np.linspace(-(np.floor(nImg-1)/2), np.floor(nImg-1)/2, nImg)
     b_ = a_.copy()
     aa, bb = np.meshgrid(a_, b_)
     rad_pix = np.sqrt(aa**2 + bb**2)
-    rad_mas = rad_pix * (mas2rad * 58.393 / (D * 1e9) )
+    rad_mas = rad_pix * (mas2rad * hlf_fov / (D * 1e9) )
     
     #  angular separation
-    aS = np.arange(nImg//2) * (mas2rad * 58.393 / (D * 1e9) )
+    aS = np.arange(nImg//2) * (mas2rad * hlf_fov / (D * 1e9) )
     
     # if lambda of interest / D
     rW_mas = (lam_c / D) * mas2rad
             
     # filename of the plot
-    fname_images_svg = 'ao_corrected_coro_psf_'+'.svg'
-    fname_images_pdf = 'ao_corrected_coro_psf_'+'.pdf'
-    fname_images_png = 'ao_corrected_coro_psf_'+'.png'
+    fname_images_svg = f'ao_corrected_coro_psf_{wvl}band'+'.svg'
+    fname_images_pdf = f'ao_corrected_coro_psf_{wvl}band'+'.pdf'
+    fname_images_png = f'ao_corrected_coro_psf_{wvl}band'+'.png'
 
     # filepath for the direct and coronagraphic images
     fpath_images_svg = fdir_plt / opd_set / fname_images_svg
@@ -282,8 +321,8 @@ for dir_nb in range(len(opds_dir)):
     iD = [0,nL//4,nL//2-1,nL*3//4,nL-1]
     
     # boundaries for the images in log scale
-    vmin0 = -5  #  .5
-    vmax0 = -1  #  0
+    vmin0 = -5
+    vmax0 = 0
     
     fig = plt.figure(4, figsize=(16,6))
     plt.clf()
@@ -301,6 +340,8 @@ for dir_nb in range(len(opds_dir)):
     
     for i in range(5):
         
+        print('position maxi in flattened psf array: ',
+              np.argmax(Int_D0[iD[i],:]))
         im = grid[i].imshow(
             np.log10(Int_D0[iD[i],:]),vmin=vmin0,vmax=vmax0,cmap='inferno',
             extent=(-aS[-1],aS[-1],-aS[-1],aS[-1]))
@@ -325,7 +366,7 @@ for dir_nb in range(len(opds_dir)):
     cbar.ax.set_ylabel('Intensity in log scale', rotation=270)
     
     plt.savefig(fpath_images_svg)
-    plt.savefig(fpath_images_pdf)
+    plt.savefig(fpath_images_pdf, bbox_inches='tight', pad_inches=0.1)
     plt.savefig(fpath_images_png)
 
     # if i==5:
