83d02af0626acd8a3ec4a4a086b286433020e2bd
spang06 <spang.alain@gmail.com>
2025-03-26 16:57:38 +0100

    update function name six_vanes to six_arms + new function six_petals

--- scripts/2D/andes/draw_vanes.py
+++ scripts/2D/andes/draw_vanes.py
@@ -4,38 +4,56 @@ Created on Mon Jul  1 17:35:17 2024
 
 @author: asp
 
-to compute mask that is six vanes with equal width
+to compute mask that is six arms with equal width
 
 """
 
 import numpy as np
 # import matplotlib.pyplot as plt
 
-def six_vanes(nPup, width):
+def six_arms(nPup, width):
     
     hlf = width/2.
     x = np.linspace(0,nPup-1,nPup)
     y = np.linspace(0,nPup-1,nPup)
     X, Y = np.meshgrid(x, y, copy=False)
     
-    vanes = np.ones((nPup,nPup))
+    arms = np.ones((nPup,nPup))
     slp = np.tan(np.pi/6.)
     oorg = nPup*(1. - np.tan(np.pi/6.))/2.
 
     dist = np.abs(Y - X*slp - oorg)/np.sqrt(1+slp*slp)
     ig = np.where(dist <= hlf)
-    vanes[:][ig] = 0.
+    arms[:][ig] = 0.
 
     oorg = nPup*(1. + np.tan(np.pi/6.))/2.
     dist = np.abs(Y + X*slp - oorg)/np.sqrt(1+slp*slp)
     ig = np.where(dist <= hlf)
-    vanes[:][ig] = 0.
+    arms[:][ig] = 0.
 
     ig = np.where(np.abs(X-nPup/2) <= hlf)
-    vanes[:][ig] = 0.
+    arms[:][ig] = 0.
     
-    # plt.imshow(vanes)
+    # plt.imshow(arms)
     
-    return vanes
+    return arms
 
+
+def six_petals(nPup):
+    
+    x = np.linspace(-nPup//2,nPup//2+1,nPup)
+    y = np.linspace(-nPup//2,nPup//2+1,nPup)
+    X, Y = np.meshgrid(x, y, copy=False)
+
+    # theta = np.arctan(Y/(X+np.sqrt(X**2. + y**2.)))*2. + np.pi/2.
+    theta = np.angle(Y+1j*X)
+    petals = np.zeros((6,nPup,nPup))
+
+    th_lim = np.linspace(-np.pi,np.pi,7)
+    for i in range(6):
+    
+        th_thr = (th_lim[i+1]+th_lim[i])/2.
+        petals[i,:] = np.where(np.abs(theta - th_thr) < np.pi/6.,1.,0.)
+    
+    return petals
 
