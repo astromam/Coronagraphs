b33ce2b04c5e721ce162776f34b4bd12d09f71ea
Mamadou <mamadou.ndiaye@oca.eu>
2025-05-30 13:03:14 +0200

    generate gif file with coronagraphic images of an offset object

---
+++ scripts/2D/andes/test_corono_andes_eta_planet_v1_movie.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 29 14:00:39 2025
+
+@author: mndiaye
+"""
+
+"""
+### Initialization
+"""
+import numpy as np
+from pathlib import Path
+import imageio as iio
+from pygifsicle import optimize
+
+#%%
+"""
+### Parameters
+"""
+# vector of angular separation for the computation of planet transmission
+sep_mas_min  = 0.0
+sep_mas_max  = 62.5 #Fmax2dbis_t[0]//2-1.
+sep_mas_stp  = 0.125  #Fmax2dbis_t[0]/nImg2dbis #0.5
+
+# array of angular separations
+nsep     = int(round(1+(sep_mas_max-sep_mas_min)/sep_mas_stp))
+duration = 10.
+
+#%%
+"""
+### Directory and filenames
+"""
+
+fdir_mov = Path('/Users/mndiaye/scratch/data/andes/movie_frames/').resolve()
+
+#%%
+"""
+### Make gif file
+"""
+frames = np.stack([iio.v3.imread(fdir_mov / f'frame_{isep:04d}.png') for isep in 4*np.arange(nsep//4)], axis = 0)
+iio.mimwrite(fdir_mov / 'movie_planet.gif', frames, fps=(nsep/4)/duration)
+
+#%%
+"""
+### Optimize gif file
+"""
+# optimize(fdir_mov / 'movie_planet.gif' , fdir_mov / "movie_planet_optimized.gif") # For creating a new one
