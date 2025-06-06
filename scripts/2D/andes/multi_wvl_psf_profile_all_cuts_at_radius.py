6c591f532b1d255bfb0b878499b917f8564b1df3
spang06 <spang.alain@gmail.com>
2025-06-06 11:25:17 +0200

    take account for spaxel size + avoid K band + better file identification

--- scripts/2D/andes/multi_wvl_psf_profile_all_cuts_at_radius.py
+++ scripts/2D/andes/multi_wvl_psf_profile_all_cuts_at_radius.py
@@ -28,8 +28,12 @@ plt.rcParams.update({'font.size': 14})  #♦  mdiaye 15!
 rad2mas = np.pi/(180.*3600*1000)
 mas2rad = 1/rad2mas
 
-as_oi = 25.
+as_oi = 20.
 as_str = str(int(as_oi))
+rW_mas = 7.
+rW_str = str(int(rW_mas))
+
+avoid_k = True
 
 #%%
 """
@@ -59,10 +63,16 @@ elif user == 'Mamadou':
     fdir_plt   = Path( fdir_base / 'plots' ).resolve()
     fdir_plt   = Path( fdir_base / 'data/OPDs_PASSATA/OPD/WS' ).resolve()
 
-was_donow = '20250305134456'
+was_donow = '20250604120819'
 fdir_res = fdir_res / was_donow
 fdir_plt = fdir_plt / was_donow
 
+
+#%%
+# 500 Hz    0.9/0.37/4.0    20250604120819
+# 1 kH0 Hz  0.9/0.37/4.0    20250602181544
+
+#%%
 # 20250305134456 perfect coro 2nd order full elt pupil
 
 # 20250121142815 yjh 0.9/0.37/4.0  0 disp,0 ncpa,0,tilt perfect: 20250122140220
@@ -146,52 +156,23 @@ fdir_plt = fdir_plt / was_donow
 ### working directory of the profile files
 """
 
-p_dir=( 'OPDs_PASSATA/OPD/WS/JQ1/20240515_163822',
-        'OPDs_PASSATA/OPD/WS/JQ1/20240517_091216',
-        'OPDs_PASSATA/OPD/WS/JQ1/20240517_100705',
-        'OPDs_PASSATA/OPD/WS/JQ1/20240517_103452',
-        'OPDs_PASSATA/OPD/WS/JQ1/20240517_105822',
-        'OPDs_PASSATA/OPD/WS/JQ1/20240517_111658',
-        'OPDs_PASSATA/OPD/WS/JQ1/20240517_113534',
-        'OPDs_PASSATA/OPD/WS/JQ1/20240517_121247',
-        'OPDs_PASSATA/OPD/WS/JQ2/20240517_181033',
-        'OPDs_PASSATA/OPD/WS/JQ2/20240517_183817',
-        'OPDs_PASSATA/OPD/WS/JQ2/20240517_190418',
-        'OPDs_PASSATA/OPD/WS/JQ2/20240517_192251',
-        'OPDs_PASSATA/OPD/WS/JQ2/20240517_194126',
-        'OPDs_PASSATA/OPD/WS/JQ2/20240517_195959',
-        'OPDs_PASSATA/OPD/WS/JQ2/20240517_201835',
-        'OPDs_PASSATA/OPD/WS/JQ2/20240517_203708',
-        'OPDs_PASSATA/OPD/WS/JQM/20240509_182041/20240509_182041.0',
-        'OPDs_PASSATA/OPD/WS/JQM/20240509_183915/20240509_183915.0',
-        'OPDs_PASSATA/OPD/WS/JQM/20240509_191620/20240509_191620.0',
-        'OPDs_PASSATA/OPD/WS/JQM/20240509_193453/20240509_193453.0',
-        'OPDs_PASSATA/OPD/WS/JQM/20240509_195327/20240509_195327.0',
-        'OPDs_PASSATA/OPD/WS/JQM/20240509_201200/20240509_201200.0',
-        'OPDs_PASSATA/OPD/WS/JQM/20240509_203033/20240509_203033.0',
-        'OPDs_PASSATA/OPD/WS/JQM/20240509_204907/20240509_204907.0',
-        'OPDs_PASSATA/OPD/WS/JQM/20240509_210742/20240509_210742.0',
-        'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-001/JQ3/20240521_200540',
-        'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-004/JQ3/20240521_181105',
-        'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-002/JQ3/20240521_213115',
-        'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-005/JQ3/20240521_222747',
-        'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-003/JQ3/20240521_210334',
-        'OPDs_PASSATA/OPD/WS/JQ3/20240527_190439/JQ3/20240527_190439',
-        'OPDs_PASSATA/OPD/WS/JQ3/20240527_195648/JQ3/20240527_195648',
-        'OPDs_PASSATA/OPD/WS/JQ3/20240527_204843/JQ3/20240527_204843',
-        'OPDs_PASSATA/OPD/WS/JQ3/20240527_214043/JQ3/20240527_214043',
-        'OPDs_PASSATA/OPD/WS/JQ3/20240527_223245/JQ3/20240527_223245',
-        'OPDs_PASSATA/OPD/WS/JQ4/20240528_161522/JQ4/20240528_161522',
-        'OPDs_PASSATA/OPD/WS/JQ4/20240528_163416/JQ4/20240528_163416',
-        'OPDs_PASSATA/OPD/WS/JQ4/20240528_165414/JQ4/20240528_165414',
-        'OPDs_PASSATA/OPD/WS/JQ4/20240528_171307/JQ4/20240528_171307',
-        'OPDs_PASSATA/OPD/WS/JQ4/20240528_173157/JQ4/20240528_173157',
-        'OPDs_PASSATA/OPD/WS/JQ4/20240528_175246/JQ4/20240528_175246',
-        'OPDs_PASSATA/OPD/WS/JQ4/20240528_181244/JQ4/20240528_181244',
-        'OPDs_PASSATA/OPD/WS/JQ4/20240528_183130/JQ4/20240528_183130',
-        'OPDs_PASSATA/OPD/WS/JQ4/20240528_185018/JQ4/20240528_185018',
-        'OPDs_PASSATA/OPD/WS/JQ4/20240528_190906/JQ4/20240528_190906')
-# p_dir=( 'OPDs_PASSATA/OPD/WS/JQM/20240509_182041/20240509_182041.0',
+# p_dir=( 'OPDs_PASSATA/OPD/WS/JQ1/20240515_163822',
+#         'OPDs_PASSATA/OPD/WS/JQ1/20240517_091216',
+#         'OPDs_PASSATA/OPD/WS/JQ1/20240517_100705',
+#         'OPDs_PASSATA/OPD/WS/JQ1/20240517_103452',
+#         'OPDs_PASSATA/OPD/WS/JQ1/20240517_105822',
+#         'OPDs_PASSATA/OPD/WS/JQ1/20240517_111658',
+#         'OPDs_PASSATA/OPD/WS/JQ1/20240517_113534',
+#         'OPDs_PASSATA/OPD/WS/JQ1/20240517_121247',
+#         'OPDs_PASSATA/OPD/WS/JQ2/20240517_181033',
+#         'OPDs_PASSATA/OPD/WS/JQ2/20240517_183817',
+#         'OPDs_PASSATA/OPD/WS/JQ2/20240517_190418',
+#         'OPDs_PASSATA/OPD/WS/JQ2/20240517_192251',
+#         'OPDs_PASSATA/OPD/WS/JQ2/20240517_194126',
+#         'OPDs_PASSATA/OPD/WS/JQ2/20240517_195959',
+#         'OPDs_PASSATA/OPD/WS/JQ2/20240517_201835',
+#         'OPDs_PASSATA/OPD/WS/JQ2/20240517_203708',
+#         'OPDs_PASSATA/OPD/WS/JQM/20240509_182041/20240509_182041.0',
 #         'OPDs_PASSATA/OPD/WS/JQM/20240509_183915/20240509_183915.0',
 #         'OPDs_PASSATA/OPD/WS/JQM/20240509_191620/20240509_191620.0',
 #         'OPDs_PASSATA/OPD/WS/JQM/20240509_193453/20240509_193453.0',
@@ -199,24 +180,90 @@ p_dir=( 'OPDs_PASSATA/OPD/WS/JQ1/20240515_163822',
 #         'OPDs_PASSATA/OPD/WS/JQM/20240509_201200/20240509_201200.0',
 #         'OPDs_PASSATA/OPD/WS/JQM/20240509_203033/20240509_203033.0',
 #         'OPDs_PASSATA/OPD/WS/JQM/20240509_204907/20240509_204907.0',
-#         'OPDs_PASSATA/OPD/WS/JQM/20240509_210742/20240509_210742.0')
+#         'OPDs_PASSATA/OPD/WS/JQM/20240509_210742/20240509_210742.0',
+#         'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-001/JQ3/20240521_200540',
+#         'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-004/JQ3/20240521_181105',
+#         'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-002/JQ3/20240521_213115',
+#         'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-005/JQ3/20240521_222747',
+#         'OPDs_PASSATA/OPD/WS/JQ3/JQ3-20240523T090047Z-003/JQ3/20240521_210334',
+#         'OPDs_PASSATA/OPD/WS/JQ3/20240527_190439/JQ3/20240527_190439',
+#         'OPDs_PASSATA/OPD/WS/JQ3/20240527_195648/JQ3/20240527_195648',
+#         'OPDs_PASSATA/OPD/WS/JQ3/20240527_204843/JQ3/20240527_204843',
+#         'OPDs_PASSATA/OPD/WS/JQ3/20240527_214043/JQ3/20240527_214043',
+#         'OPDs_PASSATA/OPD/WS/JQ3/20240527_223245/JQ3/20240527_223245',
+#         'OPDs_PASSATA/OPD/WS/JQ4/20240528_161522/JQ4/20240528_161522',
+#         'OPDs_PASSATA/OPD/WS/JQ4/20240528_163416/JQ4/20240528_163416',
+#         'OPDs_PASSATA/OPD/WS/JQ4/20240528_165414/JQ4/20240528_165414',
+#         'OPDs_PASSATA/OPD/WS/JQ4/20240528_171307/JQ4/20240528_171307',
+#         'OPDs_PASSATA/OPD/WS/JQ4/20240528_173157/JQ4/20240528_173157',
+#         'OPDs_PASSATA/OPD/WS/JQ4/20240528_175246/JQ4/20240528_175246',
+#         'OPDs_PASSATA/OPD/WS/JQ4/20240528_181244/JQ4/20240528_181244',
+#         'OPDs_PASSATA/OPD/WS/JQ4/20240528_183130/JQ4/20240528_183130',
+#         'OPDs_PASSATA/OPD/WS/JQ4/20240528_185018/JQ4/20240528_185018',
+#         'OPDs_PASSATA/OPD/WS/JQ4/20240528_190906/JQ4/20240528_190906')
+# p_dir=( 'OPDs_PASSATA/OPD/WS/JQM/20240509_182041/20240509_182041.0',
+        # 'OPDs_PASSATA/OPD/WS/JQM/20240509_183915/20240509_183915.0',
+        # 'OPDs_PASSATA/OPD/WS/JQM/20240509_191620/20240509_191620.0',
+        # 'OPDs_PASSATA/OPD/WS/JQM/20240509_193453/20240509_193453.0',
+        # 'OPDs_PASSATA/OPD/WS/JQM/20240509_195327/20240509_195327.0',
+        # 'OPDs_PASSATA/OPD/WS/JQM/20240509_201200/20240509_201200.0',
+        # 'OPDs_PASSATA/OPD/WS/JQM/20240509_203033/20240509_203033.0',
+        # 'OPDs_PASSATA/OPD/WS/JQM/20240509_204907/20240509_204907.0',
+        # 'OPDs_PASSATA/OPD/WS/JQM/20240509_210742/20240509_210742.0')
 
 # p_dir=('OPDs_PASSATA/OPD/WS/JQM/20240509_182041/20240509_123456.0',)
 
+
 #%%
 
-slc = False
-if slc:
-    ptrn = 'Lbd2D_'
-else:
-    ptrn = 'L0toD_'
+# 1 kHz
+# root_1kHz = 'OPDs_PASSATA/OPD/WS/1kHz/'
+# p_dir=(root_1kHz+'20250325_145800.0_phase_screens/20250325_145800.0',
+#           root_1kHz+'20250522_150800.0_phase_screens/20250522_150800.0',
+#           root_1kHz+'20250522_154952.0_phase_screens/20250522_154952.0_phase_screens',
+#           root_1kHz+'20250522_163144.0_phase_screens/20250522_163144.0_phase_screens',
+#           root_1kHz+'20250522_171340.0_phase_screens/20250522_171340.0_phase_screens',
+#           root_1kHz+'20250523_120216.0_phase_screens/20250523_120216.0_phase_screens',
+#           root_1kHz+'20250523_124400.0_phase_screens/20250523_124400.0_phase_screens',
+#           root_1kHz+'20250523_140738.0_phase_screens/20250523_140738.0_phase_screens',
+#           root_1kHz+'20250523_144927.0_phase_screens/20250523_144927.0_phase_screens',
+#           root_1kHz+'20250527_145633.0_phase_screens/20250527_145633.0_phase_screens',
+#           'perfect')
+
+# 500 Hz
+root_500Hz = 'OPDs_PASSATA/OPD/WS/500Hz/'
+p_dir=(root_500Hz + '20250508_163051.0_phase_screens/20250508_163051.0_oaCUBEs',
+          root_500Hz + '20250521_145220.0_phase_screens/20250521_145220.0_oaCUBEs',
+          root_500Hz + '20250521_151332.0_phase_screens/20250521_151332.0_oaCUBEs',
+          root_500Hz + '20250521_153444.0_phase_screens/20250521_153444.0_oaCUBEs',
+          root_500Hz + '20250521_155558.0_phase_screens/20250521_155558.0_oaCUBEs',
+          root_500Hz + '20250521_161711.0_phase_screens/20250521_161711.0_oaCUBEs',
+          root_500Hz + '20250521_163823.0_phase_screens/20250521_163823.0_oaCUBEs',
+          root_500Hz + '20250521_172051.0_phase_screens/20250521_172051.0_oaCUBEs',
+          root_500Hz + '20250521_174203.0_phase_screens/20250521_174203.0_oaCUBEs',
+          root_500Hz + '20250527_122032.0_phase_screens/20250527_122032.0_oaCUBEs',
+          'perfect')
+
+
+#%%
+
+# slc = False
+# if slc:
+#     ptrn = 'Lbd2D_'
+# else:
+#     ptrn = 'L0toD_'
+ptrn = ''
 
 # add noiseless plot
+# if True:
 if True:
     
     file_lst = os.listdir(fdir_res / ('perfect'))
-    
-    nncr_cnt = [x for x in file_lst if ('contrast_profile') in x]
+
+    psf_match = ['contrast_profile_', '_wonoise_', (as_str+'mas_'+rW_str)]
+    nncr_cnt = [s for s in file_lst if all([m in s for m in psf_match])]
+
+    # nncr_cnt = [x for x in file_lst if ('contrast_profile') in x]
     nncr_cnt = nncr_cnt[np.argmax(
         (lambda x:[len(i) for i in x])(nncr_cnt))]
     
@@ -226,7 +273,9 @@ if True:
     lam_min = nncr_head['LMIN']
     lam_stp = nncr_head['LSTP']
     lam_itv = nncr_head['LITV']
-    lam_lst = np.arange(lam_min,lam_min+(lam_itv+1)*lam_stp,lam_stp)
+    lam_lst = np.arange(lam_min,lam_min+(lam_itv+0.5)*lam_stp,lam_stp)
+    if avoid_k:
+        lam_lst = lam_lst[np.where(lam_lst < 1840e-9)]
     nL = len(lam_lst)
         
     nImg = nncr_head['NIMG']
@@ -247,17 +296,25 @@ for dir_nb in range(len(p_dir)):
     res_dir = (fdir_res / cur_dir)
     print(cur_dir, res_dir)
     
-    if os.path.isdir(res_dir):
+    if os.path.isdir(res_dir) and cur_dir!='perfect':
         
         file_lst = os.listdir(res_dir)
-        file_psf = [x for x in file_lst if (
-            'contrast_profile_'+ptrn+cur_dir+'_ao_corr_psf_') in x]
-        file_cro = [x for x in file_lst if (
-            'contrast_profile_'+ptrn+cur_dir+'_ao_corr_coro_psf_') in x]
-                
+        # file_psf = [x for x in file_lst if (
+        #     'contrast_profile_'+ptrn+cur_dir+'_ao_corr_psf_') in x]
+        # file_cro = [x for x in file_lst if (
+        #     'contrast_profile_'+ptrn+cur_dir+'_ao_corr_coro_psf_') in x]
+
+        psf_match = ['contrast_profile_', '_ao_corr_psf_',
+                     (as_str+'mas_'+rW_str)]
+        file_psf = [s for s in file_lst if all([m in s for m in psf_match])]
+
+        cro_match = ['contrast_profile_', '_ao_corr_coro_psf_',
+                     (as_str+'mas_'+rW_str)]
+        file_cro = [s for s in file_lst if all(m in s for m in cro_match)]
+     
         print("psf :", file_psf)
         print("psf coro :", file_cro)
-    
+        
         Int_D0_prf_avg = fits.getdata(res_dir / file_psf[0])
         Int_D_prf_avg  = fits.getdata(res_dir / file_cro[0])
         Int_D_prf_std  = fits.getdata(res_dir / file_cro[0], ext=1)
@@ -272,7 +329,9 @@ for dir_nb in range(len(p_dir)):
             lam_min = head_psf['LMIN']
             lam_stp = head_psf['LSTP']
             lam_itv = head_psf['LITV']
-            lam_lst = np.arange(lam_min,lam_min+(lam_itv+1)*lam_stp,lam_stp)
+            lam_lst = np.arange(lam_min,lam_min+(lam_itv+0.5)*lam_stp,lam_stp)
+            if avoid_k:
+                lam_lst = lam_lst[np.where(lam_lst < 1840e-9)]
             nL = len(lam_lst)
             
             nImg = head_psf['NIMG']
@@ -284,25 +343,25 @@ for dir_nb in range(len(p_dir)):
             aS = np.arange(nImg//2)*(mas2rad * hlf_fov / (D *1e9) ) # ang. sep.
             pos_as_oi = int(np.median(np.argmin(np.abs(aS[:]-as_oi))))
              
-            try:
-                disp = head_psf['DISP']
-            except KeyError:
-                disp=0
+            # try:
+            #     disp = head_psf['DISP']
+            # except KeyError:
+            #     disp=0
             
-            try:
-                ncpa = head_psf['NCPA']
-            except KeyError:
-                ncpa=0
+            # try:
+            #     ncpa = head_psf['NCPA']
+            # except KeyError:
+            #     ncpa=0
         
-            try:
-                offset = head_psf['FDEC']
-            except KeyError:
-                offset=0
+            # try:
+            #     offset = head_psf['FDEC']
+            # except KeyError:
+            #     offset=0
          
-            try:
-                ls_ape = head_psf['LSAE']
-            except KeyError:
-                ls_ape=0
+            # try:
+            #     ls_ape = head_psf['LSAE']
+            # except KeyError:
+            #     ls_ape=0
                    
             # disp = head_psf['DISP']
             # ncpa_rms = head_psf['NCPA']
@@ -389,15 +448,20 @@ plt.tight_layout()
 plt.xlabel(r'Wavelength $\lambda$ [nm]')#[$\lambda$/D]')
 plt.ylabel(f'Contrast @ {int(as_oi)} mas')
 plt.yscale('log')
-tilt = np.round(offset*mas2rad,1)
+# tilt = np.round(offset*mas2rad,1)
 # plt.title(f'ls off. 0.0°, tilt {tilt} mas, ncpa {int(ncpa)} nm rms, disp. {int(disp*1e-6)} mas/µm')
 plt.title('Coronagraph configuration for YJH band')
 plt.grid(True)
-for i in range(5):
+i = 2
+if i==2:
+# for i in range(5):
     
-    idx = [j for j,item in enumerate(np.array(p_dir)) if jqs[i] in item]
+    i=2
+    idx = i # [j for j,item in enumerate(np.array(p_dir)) if jqs[i] in item]
     
     if idx != []:
+        
+        idx = np.arange(len(p_dir)-1)
     
         c_avg = np.mean(contrast[:,idx], axis=1)
         c_std = np.max(cont_std[:,idx], axis=1)
@@ -407,7 +471,7 @@ for i in range(5):
         c_max = np.max(contrast[:,idx], axis=1)
 
         plt.plot(lam_lst*1e9, c_avg,
-                 label=seeings[jqs[i]] + ' (' + jqs[i] + ')',
+                 # label=seeings[jqs[i]] + ' (' + jqs[i] + ')',
                  color=c_tab[i])
         plt.fill_between(lam_lst*1e9, c_min, c_max, alpha=0.2, color=c_tab[i])
         
@@ -416,34 +480,41 @@ for i in range(5):
             c_std_avg = np.mean(cont_std[:,idx], axis=1)
             c_std_std = np.std(cont_std[:,idx], axis=1)
             idxm = idx.copy()
-            fnm = (fdir_res / ('contrast_'+jqs[i]+'_'+was_donow+'.fits'))
-            fits.writeto(fnm, np.array((c_avg,c_std)), overwrite=True)
+            plt.plot(lam_lst*1e9, np.mean(gain[:,idxm]*contrast[:,idxm],axis=1),
+                      ls='--', color='black')
+
+            fnm = (fdir_res / 
+                   ('contrast_'+str(int(as_oi))+'mas_'+str(int(rW_mas))+'mas_'+
+                    jqs[i]+'_'+was_donow+'.fits'))
+            fits.writeto(fnm, np.array((c_avg,lam_lst)), head_psf, overwrite=True)
             fits.append(fnm, np.array((c_min, c_max)), overwrite=True)
             fits.append(fnm, np.array((np.mean(gain[:,idx], axis=1),
                                         np.std(gain[:,idx], axis=1))),
-                        overwrite=True)
-           
+                        overwrite=True)         
 
 plt.plot(lam_lst*1e9, contrast_nncr, color='black', ls=':')
 # plt.plot(lam_lst*1e9, np.mean(gain[:,idxm]*contrast[:,idxm],axis=1),
 #           ls='--', color='black')
 # , label='median atm., no coro.')
 bbox = dict(boxstyle='square', fc='w', alpha=0.75)
-# plt.text(1980,9e-2,'---- atmo., no coro.', bbox=bbox)
-plt.text(1000,2e-5,'... no atmo., coro.', bbox=bbox)
+plt.text(int(lam_lst[-1]*1e9 - 250),2e-2,'---- atmo., no coro.', bbox=bbox)
+plt.text(1000,9e-5,'... no atmo., coro.', bbox=bbox)
 
 plt.ylim(1e-5,1e-1)
-plt.legend(title='Seeing',fontsize='small', loc=4)
+# plt.legend(title='Seeing',fontsize='small', loc=4)
 
-fname_contrast_25mas = (
-    f'all_windshake_data_contrast_{int(as_oi)}mas_as_10masRingAvgdPrfs_vs_wvl_' +
+fname_contrast_as_oi = (
+    f'all_windshake_data_contrast_{int(as_oi)}mas_as_{int(rW_mas)}masRingAvgdPrfs_vs_wvl_' +
+                    os.path.basename(file_cro[0]).split('.')[0])
+fname_contrast_as_oi = (
+    f'JQM_data_contrast_{int(as_oi)}mas_as_{int(rW_mas)}masRingAvgdPrfs_vs_wvl_' +
                     os.path.basename(file_cro[0]).split('.')[0])
 
-# fname_contrast_25mas = ('median_condition_data_contrast_' + as_str +'mas_'+ was_donow)
+# fname_contrast_as_oi = ('median_condition_data_contrast_' + as_str +'mas_'+ was_donow)
 
-fpath_contrast_25mas_svg = fdir_plt / (fname_contrast_25mas + '.svg')
-fpath_contrast_25mas_pdf = fdir_plt / (fname_contrast_25mas + '.pdf')
-fpath_contrast_25mas_png = fdir_plt / (fname_contrast_25mas + '.png')
+fpath_contrast_25mas_svg = fdir_plt / (fname_contrast_as_oi + '.svg')
+fpath_contrast_25mas_pdf = fdir_plt / (fname_contrast_as_oi + '.pdf')
+fpath_contrast_25mas_png = fdir_plt / (fname_contrast_as_oi + '.png')
 
 plt.savefig(fpath_contrast_25mas_svg)
 plt.savefig(fpath_contrast_25mas_pdf, bbox_inches='tight', pad_inches=0.1)
