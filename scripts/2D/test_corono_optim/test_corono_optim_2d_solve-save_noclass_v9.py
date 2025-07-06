e1c253c9f46a0a359d4f984ad1e89c41c113fd7c
Mamadou <mamadou.ndiaye@oca.eu>
2025-07-06 13:02:17 +0200

    change pupil size to 400 for simulations

--- scripts/2D/test_corono_optim/test_corono_optim_2d_solve-save_noclass_v9.py
+++ scripts/2D/test_corono_optim/test_corono_optim_2d_solve-save_noclass_v9.py
@@ -51,7 +51,7 @@ FirstDerGlobalLim = 100.
 BinarityReg       = 0.1
 
 #nPup = corono0.params['nPup']
-nPup0 = 506 #512
+nPup0 = 400 # 506 #512
 nExt0 = 0
 nDim0 = nPup0 + nExt0
 nFPM = 50
@@ -113,6 +113,7 @@ if do_dead_act:
     
 
 do_fits = True
+
 
 #%%
 """
