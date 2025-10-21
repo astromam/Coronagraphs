16cddeb146ebfc6f0ee54f85c9a896ef02e2a62f
spang06 <spang.alain@gmail.com>
2025-10-21 16:14:16 +0200

    better fourier plane definition and formatting

--- scripts/2D/andes/test_corono_andes_multi_wvl_tilt_perfect.py
+++ scripts/2D/andes/test_corono_andes_multi_wvl_tilt_perfect.py
@@ -45,26 +45,34 @@ nPup = 400  #  even/pair!
 nFPM = 100
 
 # half field of view in mas
-hlf_fov = 58.393
+# hlf_fov = 58.393 # some legacy value...
 
 # Image size
 nImg = 400  #*1.6 #  #  even/pair!
 
 # angular separation of interest in mas
-as_oi = 25.
+as_oi = 20.
 
 # # of phase screens
-nOPD = 2000
+nOPD = 4000
 
-# wavelengths in m
 lamC = 1600e-9  #  some reference wvl unique value
-lam_min = 960e-9  #  min value in range
-lam_max = 2450e-9  #  max value in range  #  2450e-9 // 1800e-9
-lam_itv = 18  #  nb of intervals in range --> nb+1 wvl's !  #  18 // 10
-lam_stp = np.floor(np.ceil((lam_max-lam_min)*1e9/lam_itv)/10)*1e-8  # wvl step
-lam_lst = np.arange(lam_min,lam_max,lam_stp) if lam_max != lam_min else [lamC]
-nL = len(lam_lst)
 
+# wavelengths in m --> 2025-03
+# lam_min = 960e-9  #  min value in range
+# lam_max = 2450e-9  #  max value in range  #  2450e-9 // 1800e-9
+# lam_itv = 18  #  nb of intervals in range --> nb+1 wvl's !  #  18 // 10
+# lam_stp = np.floor(np.ceil((lam_max-lam_min)*1e9/lam_itv)/10)*1e-8  # wvl step
+# lam_lst = np.arange(lam_min,lam_max,lam_stp) if lam_max != lam_min else [lamC]
+
+# yjhk  2025-05 -->
+lam_min = 950e-9  #  min value in range
+lam_itv = 30
+lam_stp = 50e-9
+
+lam_lst = np.arange(lam_min,lam_min+(lam_itv+0.5)*lam_stp,lam_stp)
+
+nL = len(lam_lst)
 print(nL, lam_stp, lam_lst)
 
 # Pupil diameter in m 
@@ -79,19 +87,27 @@ pscale = 0.3
 
 lamCD2mas = (lamC/D)*mas2rad
 
+# field of view
+# in mas
+fov_mas = nImg * pscale
+# in radians
+fov_rdn = fov_mas * rad2mas
+# in multiple of reference lambda (lamC) over D
+mD_ref = fov_rdn / ( lamC / D )
+
 """
 ### Coronagraphic components
 """
 # Focal plane mask
 mask2d = uniform_disk(nFPM, nFPM/2.)
-# diam = 0.90 obs = 0.37, mB = 4.5 with 'ELT_pupil_400.fits' YJH
+# diam = 0.90 obs = 0.37, mB = 4. with 'ELT_pupil_400.fits' YJH
 # diam = 0.96 obs = 0.30, mB = 4.5 with 'ELT_pupil_400.fits' HK
 # diam = 0.90 obs = 0.38 with 'Tel-Pupil.fits'
 # diam = 0.90 # diameter of the pupil in fraction of the pupil size
 # obst = 0.37 # diameter of the central obscuration in fraction of the pupil size
-diam = 1.0
-obst = 0.0
-mB = 0.
+diam = 0.90
+obst = 0.37
+mB = 4.
 # FPM size in lam/D in the focal plane B
 # 4. with 'ELT_pupil_400.fits'
 # 3.8 with 'Tel-Pupil.fits', old!
@@ -178,10 +194,12 @@ LyotStop2d = Pupil*(
 
 #%%
 
-opd_set='diam_'+str(diam)+'-obst_'+str(obst)+'_FPM_lcToD_'+str(mB)
-os.makedirs(fdir_res / opd_set, exist_ok=True)
-os.makedirs(fdir_plt / opd_set, exist_ok=True)
-    
+# opd_set='diam_'+str(diam)+'-obst_'+str(obst)+'_FPM_lcToD_'+str(mB)
+# os.makedirs(fdir_res / opd_set, exist_ok=True)
+# os.makedirs(fdir_plt / opd_set, exist_ok=True)
+os.makedirs(fdir_res , exist_ok=True)
+os.makedirs(fdir_plt , exist_ok=True)
+
 
 #%%
 
@@ -196,8 +214,9 @@ for i in np.arange(nL):
     lamD2mas = (lam/D)*mas2rad
     
     # FoV in lam/D in the final image plane D
-    mD = hlf_fov*(nImg/(lam*1e9))
-    
+    # mD = hlf_fov*(nImg/(lam*1e9))
+    mD = mD_ref * lamC / lam
+
     print('lambda (nm):' ,np.round(lam*1e9,0), '. Field of view (lam/D):',
           np.round(mD,3))
     
@@ -208,7 +227,7 @@ for i in np.arange(nL):
     """
     
     # Field in the entrance pupil plane A
-    Fld_AA0 = Pupil * 1. #*LyotStop2d
+    Fld_AA0 = Pupil * 1. * LyotStop2d
     
     # Field in the image plane D (no coronagraph)
     Fld_DD0 = sft.sft(Fld_AA0, nImg, mD*diam)
@@ -232,10 +251,10 @@ for i in np.arange(nL):
     Fld_BB = mask2d*sft.sft(Fld_AA, nFPM, mB*lamC/lam)
     
     # pupil plane C before Lyot stop
-    Fld_CC = Fld_AA # - sft.isft(Fld_BB, nPup, mB*lamC/lam)
+    Fld_CC = Fld_AA - sft.isft(Fld_BB, nPup, mB*lamC/lam)
     
     # # pupil plane C after Lyot stop
-    Fld_LL = Fld_CC #*LyotStop2d
+    Fld_LL = Fld_CC * LyotStop2d
     
     # image plane D 
     Fld_DD = sft.sft(Fld_LL, nImg, mD*diam)
@@ -254,8 +273,8 @@ for i in np.arange(nL):
     
     
     # computation of the standard deviation intensity profiles of the images
-    Int_DD0_prf_std, rad_DD0_prf_std = profile(Int_DD0[i,:], ptype='std')
-    Int_DD_prf_std, rad_DD_prf_std = profile(Int_DD[i,:], ptype='std')
+    # Int_DD0_prf_std, rad_DD0_prf_std = profile(Int_DD0[i,:], ptype='std')
+    # Int_DD_prf_std, rad_DD_prf_std = profile(Int_DD[i,:], ptype='std')
     
     """
     ### Compute the radial intensity profiles of the images
@@ -283,8 +302,8 @@ fname_Int_DD = 'wonoise_coro_psf_'+donow+'.fits'
 
 
 # filepath for the direct and coronagraphic images
-fpath_Int_DD0 = fdir_res / opd_set / fname_Int_DD0
-fpath_Int_DD  = fdir_res / opd_set / fname_Int_DD
+fpath_Int_DD0 = fdir_res / fname_Int_DD0
+fpath_Int_DD  = fdir_res / fname_Int_DD
 
 # save the direct and coronagraphic images
 fits.writeto(fpath_Int_DD0, Int_DD0, overwrite=True)
@@ -297,7 +316,7 @@ for fpath in fpath_psf_lst:
     fits.setval(fpath,'NFPM',value=nFPM,comment='FP coro. sampling')
     fits.setval(fpath,'NIMG',value=nImg,comment='image size')
     fits.setval(fpath,'NOPD',value=nOPD,comment='number of OPD files')
-    fits.setval(fpath,'FOVS',value=hlf_fov,comment='field of view in as')
+    fits.setval(fpath,'FOVS',value=fov_mas,comment='field of view in as')
     fits.setval(fpath,'LMIN',value=lam_min,comment='wavelength in meters')
     fits.setval(fpath,'LITV',value=lam_itv,comment='# of wvl intervals')
     fits.setval(fpath,'LSTP',value=lam_stp,comment='wvl step in meters')
@@ -310,6 +329,27 @@ for fpath in fpath_psf_lst:
     fits.setval(fpath,'DISP',value=0.,comment='achr. disp. in mas/m bw')
     fits.setval(fpath,'NCPA',value=0.,comment='ncpa rms in meters')
     fits.setval(fpath,'FDEC',value=0.,comment='psf to fpm offset in radians')
+    fits.setval(fpath,'OPDS',value='',comment='opd set creation date')
+    fits.setval(fpath,'DISP',value=disp,comment='achr. disp. in mas/m bw')
+    fits.setval(fpath,'NCPA',value=0,comment='ncpa rms in meters')
+    fits.setval(fpath,'FDEC',value=0,
+                comment='psf to fpm offset in radians')
+    fits.setval(fpath,'FTLT',value=0,
+                comment='psf to fpm offset in mas')
+    fits.setval(fpath,'LSAE',value=0,
+                comment='lyot stop angular position error in degrees')
+    fits.setval(fpath,'LSVE',value=0,
+                comment='lyot stop vertical offset error in pixels')
+    fits.setval(fpath,'LSHE',value=0,
+                comment='lyot stop horizontal offset error in pixels')
+    fits.setval(fpath,'ELT_DFOC',value=0,
+                comment='defocus at elt pupil in nm RMS at reference wvl')
+    fits.setval(fpath,'DIAM_DFC',value=np.round(0,3),
+                comment='pup. circumcirc. input defocus nm RMS ref. wvl')
+    fits.setval(fpath,'EPUP_FNM',value=fname_elt,
+            comment='ELT pupil filename')
+    fits.setval(fpath,'DATE_NOW',value=donow,
+            comment='daye of now, i.e. script execution')
 
 
 
@@ -320,8 +360,8 @@ plot profiles
 
 fname_prf_svg = 'intensities_profiles_wo_noise'+donow+'.svg'
 fname_prf_pdf = 'intensities_profiles_wo_noise'+donow+'.pdf'
-fpath_prf_svg = fdir_plt / opd_set / fname_prf_svg
-fpath_prf_pdf = fdir_plt / opd_set / fname_prf_pdf
+fpath_prf_svg = fdir_plt / fname_prf_svg
+fpath_prf_pdf = fdir_plt / fname_prf_pdf
 
 # plot of the radial profiles
 colors = plt.cm.rainbow(np.linspace(0,1,nL))
@@ -353,4 +393,6 @@ plt.savefig(fpath_prf_svg)
 plt.savefig(fpath_prf_pdf)
     
 plt.show()
+
+print('date of now : ', donow)
 
